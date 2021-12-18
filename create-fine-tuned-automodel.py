8fab7b915334790da9f514f4522d59180b440278
LucasLopesSI <lucaslopes.si@usp.br>
2021-12-18 12:37:35 -0300

    Increased number of training sentences to 8600

--- create-fine-tuned-automodel.py
+++ create-fine-tuned-automodel.py
@@ -174,7 +174,7 @@ def optimizeMlmModel(trainingSentences, percentageOfMaskedTokens):
   print(loss.item())
 
 cont = 0
-while cont < 3:
+while cont < 8600:
   optimizeMlmModel(abstractTrainingSentences[cont:cont+1], 0.2)
   cont+=1
 
